d98ee9f762061b6851310e1d24d1b43aed153fa8
McCrog <pit_mib@bk.ru>
2018-02-25 13:30:29 +0300

    Task 2. Allow the user to change sort order via a setting

--- app/src/main/java/com/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/popularmovies/MainActivity.java
@@ -1,10 +1,14 @@
 package com.udacity.popularmovies;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.udacity.popularmovies.model.Movie;
@@ -25,8 +29,10 @@ public class MainActivity extends AppCompatActivity {
     private final static String API_KEY = "";
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    RecyclerView recyclerView;
-    List<Movie> movies;
+    private RecyclerView recyclerView;
+    private static List<Movie> movies;
+
+    private boolean sortByPopular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +48,55 @@ public class MainActivity extends AppCompatActivity {
 
         movies = new ArrayList<>();
 
-        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycle_view);
+        recyclerView = (RecyclerView) findViewById(R.id.movies_recycle_view);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
+        defaultSetup();
+
+        getData();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                getData();
+                return true;
+            case R.id.sort_by:
+                return true;
+            case R.id.sort_by_popular:
+                sortByPopular = true;
+                item.setChecked(true);
+                getData();
+                return true;
+            case R.id.sort_by_top_rated:
+                sortByPopular = false;
+                item.setChecked(true);
+                getData();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void getData() {
         MoviesAPIService moviesApiService = ApiClient.getClient().create(MoviesAPIService.class);
 
-        Call<MoviesResponse> popularMovies = moviesApiService.getPopularMovies(API_KEY);
+        Call<MoviesResponse> popularMovies;
+        if (sortByPopular) {
+            popularMovies = moviesApiService.getPopularMovies(API_KEY);
+        } else {
+            popularMovies = moviesApiService.getTopRatedMovies(API_KEY);
+        }
+
         popularMovies.enqueue(new Callback<MoviesResponse>() {
             @Override
             public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
@@ -61,7 +110,13 @@ public class MainActivity extends AppCompatActivity {
                 // Log error here since request failed
                 Log.e(TAG, t.toString());
                 Toast.makeText(MainActivity.this, "An error occurred during networking", Toast.LENGTH_SHORT).show();
+                // TODO Add text view for error
             }
         });
+    }
+
+    private void defaultSetup() {
+        Resources res = getResources();
+        sortByPopular = res.getBoolean(R.bool.movie_sort_by_popular);
     }
 }
